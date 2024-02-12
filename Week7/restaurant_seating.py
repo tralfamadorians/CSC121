e7e712128c76d17cc49caea7d1a49e6bf1a36556
tralfamadorians <kellyincville@gmail.com>
2024-02-12 11:57:01 -0600

    Week 7.

---
+++ Week7/restaurant_seating.py
@@ -0,0 +1,11 @@
+'''7-2. Restaurant Seating: Write a program that asks the user how many people
+
+are in their dinner group. If the answer is more than eight, print a message say-
+ing they’ll have to wait for a table. Otherwise, report that their table is ready.'''
+
+number = int(input("How many people are in your dinner group? "))
+
+if number > 8:
+    print("You'll have to wait for a table.")
+else:
+    print("Your table is ready.")
