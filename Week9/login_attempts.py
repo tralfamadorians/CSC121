6b2e4dfb38a205990420b4f8a57251145ea3b983
tralfamadorians <kellyincville@gmail.com>
2024-03-25 08:42:49 -0500

    Week 9

--- Week9/login_attempts.py
+++ Week9/login_attempts.py
@@ -1,12 +1,3 @@
-'''9-5. Login Attempts: Add an attribute called login_attempts to your User class
-from Exercise 9-3 (page 162). Write a method called increment_login_attempts()
-that increments the value of login_attempts by 1. Write another method called
-reset_login_attempts() that resets the value of login_attempts to 0.
-Make an instance of the User class and call increment_login_attempts()
-several times. Print the value of login_attempts to make sure it was incremented
-properly, and then call reset_login_attempts(). Print login_attempts again to
-make sure it was reset to 0.'''
-
 class User:
     def __init__(self, first_name, last_name, city, state, age):
         # Store attributes about an user
@@ -43,10 +34,6 @@ user2 = User('walter', 'meyers', 'mathews', 'VA', 72)
 user3 = User('antonio', 'rivera', 'badajoz', 'SP', 90)
 
 users = [user0, user1, user2, user3]
-
-for user in users:
-    user.describe_user()
-    user.greet_user()
 
 for i in range(4):
     user0.increment_login_attempts()
--- Week9/lottery.py
+++ Week9/lottery.py
@@ -1,18 +1,41 @@
-'''9-14. Lottery: Make a list or tuple containing a series of 10 numbers and 5 letters.
-Randomly select 4 numbers or letters from the list and print a message saying that
-any ticket matching these 4 numbers or letters wins a prize.'''
-
-from random import  choice
+from random import choice
 
 my_list = ['A', 'Y', '0', 'E', '1', 'T', '2', 'Z', '3', '4',
            '5', '6', '7', '8', '9']
 random_list = []
 
-for count in range(4):
-    # Randomly choose four items and move them from the original list to the new one
+count = 0
+while count < 4:
+    # Randomly choose four numbers and move them from the original list to the new one
     item = choice(my_list)
-    my_list.remove(item)
-    random_list.append(item)
-    count +=1
+    if item.isdigit():
+        my_list.remove(item)
+        random_list.append(item)
+        count +=1
 
-print(f"Any ticket matching the following four numbers or letters wins a prize!\n {random_list}")
+while True:
+    # Randomly chooses one letter and moves it from the original list to the new one
+    item = choice(my_list)
+    if item.isdigit() is False:
+        my_list.remove(item)
+        random_list.append(item)
+        break
+
+guess = input(f"Please type in your selected four numbers and one letter: ")
+guess_list = [char for char in guess]
+
+random_list_copy = random_list.copy()   # Copy to iterate over
+guess_list_copy = guess_list.copy()     # Copy to iterate over
+
+for guesses in guess_list_copy:
+    # Check for matching numbers and letter and remove them if a matching pair is found
+    for win in random_list_copy:
+        if guesses == win:
+            guess_list.remove(guesses)
+            random_list.remove(win)
+ 
+if not guess_list:  # If all were removed, then the guess_list was a win
+    print("You won! Congratulations!")
+else:
+    print("Unfortunately, you didn't win this time.")
+    
