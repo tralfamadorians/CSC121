887f7540f31890b6323739ef13c87f34ddebbc2e
tralfamadorians <kellyincville@gmail.com>
2024-03-31 13:11:57 -0500

    Week 10

--- Week10/addition.py
+++ Week10/addition.py
@@ -1,10 +1,3 @@
-'''10-6. Addition: One common problem when prompting for numerical input
-occurs when people provide text instead of numbers. When you try to convert
-the input to an int, you’ll get a ValueError. Write a program that prompts for
-two numbers. Add them together and print the result. Catch the ValueError if
-either input value is not a number, and print a friendly error message. Test your
-program by entering two numbers and then by entering some text instead of a
-number.'''
 
 # Ask user for two numbers
 number1 = input("Please input a number: ")
