47dbfae59e94b8eb9a0f7e8abd27badb96a3ded2
tralfamadorians <kellyincville@gmail.com>
2024-04-14 19:58:19 -0500

    Week 11

--- Week10/addition_calculator.py
+++ Week10/addition_calculator.py
@@ -4,19 +4,19 @@ enter text instead of a number.'''
 
 # Ask user for two numbers
 number1 = input("Please input a number: ")
-number2 = input("Please input a second number: ")
+number2 = input("Please input another number: ")
 
 while not number1.isdigit():
     try:
         number1 = int(number1)
     except ValueError:
-        print(f"{number1} is not a number.")
+        number1 = input(f"{number1} is not a number. Please input a number. ")
 
 while not number2.isdigit():
     try:
-        number2 = int(number1)
+        number2 = int(number2)
     except ValueError:
-        print(f"{number2} is not a number.")
+        number2 = input(f"{number2} is not a number. Please input a number. ")
 
 # Print sum 
 print(f"The sum of the two numbers are {int(number1) + int(number2)}.")
